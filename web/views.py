dac7dba3d27164bcd68e2f3d87c42823b032d6c1
Miguel Yupanqui <myupanquirondo@gmail.com>
2026-03-29 14:18:22 -0500

    fix: Corregido el enlace y filtrado de productos por categoría en la vista principal

--- web/views.py
+++ web/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
+from .models import Categoria, Producto
+
 # Create your views here.
+""" VISTAS PARA EL CATALOGO DE PRODUCTOS """
+def index(request):
+    listaProductos = Producto.objects.all()
+    listaCategorias = Categoria.objects.all()
+
+    context = {
+        'productos': listaProductos,
+        'categorias': listaCategorias
+    }
+    return render(request, 'index.html',context)
+
+
+""" vista para filtrar productos por categoria """
+def productosPorCategoria(request,categoria_id):
+    objCategoria = Categoria.objects.get(pk=categoria_id)
+    listaProductos = objCategoria.producto_set.all()
+
+    listaCategorias = Categoria.objects.all()
+    context = {
+        'categorias':listaCategorias,
+        'productos':listaProductos
+    }
+    return render(request, 'index.html',context)
