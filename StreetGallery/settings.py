7b41c34b482b3f8888b618f79d2f0ceed8862ada
Miguel Yupanqui <myupanquirondo@gmail.com>
2026-03-28 22:23:38 -0500

    feat: configuración inicial del admin con Django Unfold y estructura base del proyecto

--- StreetGallery/settings.py
+++ StreetGallery/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -31,6 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "unfold",
+    "web",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -74,8 +77,12 @@ WSGI_APPLICATION = "StreetGallery.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "db_StreetGallery",
+        "USER": "root",
+        "PASSWORD": "123",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
     }
 }
 
@@ -120,3 +127,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Categoria,Producto
+
+admin.site.register(Categoria)
+
+@admin.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'precio', 'categoria', 'fecha_registro')
+    list_editable = ('precio',)
