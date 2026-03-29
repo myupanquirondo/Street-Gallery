d4dbcd9079d66cac0554eb64b1abaf7a0e3a3705
Miguel Yupanqui <myupanquirondo@gmail.com>
2026-03-29 15:22:12 -0500

    feat: mostrar enlaces de imagen y nombre para ir a la página de detalle del producto

--- web/urls.py
+++ web/urls.py
@@ -7,4 +7,6 @@ app_name = 'web'
 urlpatterns = [
     path('', views.index, name='index'),
     path('productosPorCategoria/<int:categoria_id>',views.productosPorCategoria, name='productosPorCategoria'),
+    path('productosPorNombre',views.productosPorNombre, name='productosPorNombre'),
+    path('producto/<int:producto_id>/', views.productoDetalle, name='producto'),
 ]
--- web/views.py
+++ web/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Categoria, Producto
 
@@ -26,3 +26,25 @@ def productosPorCategoria(request,categoria_id):
         'productos':listaProductos
     }
     return render(request, 'index.html',context)
+
+
+""" vista para filtrado de productos por nombre """
+def productosPorNombre(request):
+    nombre = request.POST['nombre']
+
+    listaProductos = Producto.objects.filter(nombre__contains=nombre)
+    listaCategorias = Categoria.objects.all()
+    context = {
+        'categorias':listaCategorias,
+        'productos':listaProductos
+    }
+    return render(request, 'index.html',context)
+
+
+""" vista para el detalle de producto"""
+def productoDetalle(request,producto_id):
+    objProducto = get_object_or_404(Producto,pk=producto_id)
+    context = {
+        'producto':objProducto
+    }
+    return render(request, 'producto.html',context)
