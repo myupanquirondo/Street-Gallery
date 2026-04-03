92128d9dfaaa15d020a3765a8f92ba58586502e5
Miguel Yupanqui <myupanquirondo@gmail.com>
2026-04-03 14:45:50 -0500

    Refactor: Se organizo el settings en carpetas y configurar archivos estaticos para produccion local

--- StreetGallery/urls.py
+++ StreetGallery/urls.py
@@ -16,13 +16,24 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path, include, re_path # Añadimos re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('', include('web.urls')),
     path("admin/", admin.site.urls),
     path('paypal/', include("paypal.standard.ipn.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+# Si estás en modo desarrollo (DEBUG = True)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    # Si estás en modo producción (DEBUG = False)
+    # Usamos re_path para que la expresión regular funcione correctamente
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+        re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    ]
